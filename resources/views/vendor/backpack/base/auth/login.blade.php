eea2fda01a26ba28f9b3c22be1cb6b2c391738d8
Zulfikri Zulkiflee <zulfikrizulkiflee@yahoo.com.my>
2017-10-11 11:52:41 +0800

    auth css & page

--- resources/views/vendor/backpack/base/auth/login.blade.php
+++ resources/views/vendor/backpack/base/auth/login.blade.php
@@ -1,8 +1,8 @@
 @extends('backpack::layout')
-
+<link rel="stylesheet" href="{{ asset('vendor/adminlte/') }}/dist/css/skins/custom.css">
 @section('content')
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-6 col-md-offset-3 vertical-center">
             <div class="box box-default">
                 <div class="box-header with-border">
                     <div class="box-title">{{ trans('backpack::base.login') }}</div>
@@ -54,6 +54,9 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ trans('backpack::base.login') }}
                                 </button>
+                                <a href="{{ url(config('backpack.base.route_prefix', 'admin').'/register') }}"><button type="button" class="btn btn-primary">
+                                    {{ trans('backpack::base.register') }}
+                                </button></a>
 
                                 <a class="btn btn-link" href="{{ url(config('backpack.base.route_prefix', 'admin').'/password/reset') }}">{{ trans('backpack::base.forgot_your_password') }}</a>
                             </div>
--- resources/views/vendor/backpack/base/layout.blade.php
+++ resources/views/vendor/backpack/base/layout.blade.php
@@ -23,6 +23,7 @@
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/') }}/dist/css/AdminLTE.min.css">
     <!-- AdminLTE Skins. Choose a skin from the css/skins folder instead of downloading all of them to reduce the load. -->
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/') }}/dist/css/skins/_all-skins.min.css">
+    <!--For customs css in login & register, located inside login & register file-->
 
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/') }}/plugins/pace/pace.min.css">
     <link rel="stylesheet" href="{{ asset('vendor/backpack/pnotify/pnotify.custom.min.css') }}">
