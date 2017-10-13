6e5b115bb9b5629e58c009e4975596f4bf09db5b
Zulfikri Zulkiflee <zulfikrizulkiflee@yahoo.com.my>
2017-10-13 12:31:00 +0800

    project name update

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -12,11 +12,11 @@ return [
     */
 
     // Project name. Shown in the breadcrumbs and a few other places.
-    'project_name' => 'Backpack',
+    'project_name' => 'iHome',
 
     // Menu logos
-    'logo_lg'   => '<b>Back</b>pack',
-    'logo_mini' => '<b>B</b>p',
+    'logo_lg'   => '<b>i</b>Home',
+    'logo_mini' => '<b>i</b>H',
 
     // Developer or company name. Shown in footer.
     'developer_name' => 'Cristian Tabacitu',
