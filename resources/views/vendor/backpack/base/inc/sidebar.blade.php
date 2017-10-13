f9f0d7f2eebc86014a403cac02e7e8d7044bbc1d
Zulfikri Zulkiflee <zulfikrizulkiflee@yahoo.com.my>
2017-10-13 17:04:26 +0800

    css update

--- resources/views/vendor/backpack/base/inc/sidebar.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar.blade.php
@@ -39,8 +39,10 @@
           <li><a href="{{ url(config('backpack.base.route_prefix', 'admin') . '/elfinder') }}"><i class="fa fa-files-o"></i> <span>File manager</span></a></li>
 
           <!-- ======================================= -->
+<!--
           <li class="header">{{ trans('backpack::base.user') }}</li>
           <li><a href="{{ url(config('backpack.base.route_prefix', 'admin').'/logout') }}"><i class="fa fa-sign-out"></i> <span>{{ trans('backpack::base.logout') }}</span></a></li>
+-->
         </ul>
       </section>
       <!-- /.sidebar -->
