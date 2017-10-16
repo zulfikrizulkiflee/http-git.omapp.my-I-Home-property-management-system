a0da4f00c8c01f0259294124dd972c0c9794b1bc
Zulfikri Zulkiflee <zulfikrizulkiflee@yahoo.com.my>
2017-10-16 18:33:12 +0800

    search cards

--- resources/views/admin/search/list.blade.php
+++ resources/views/admin/search/list.blade.php
@@ -1,5 +1,13 @@
 @extends('backpack::layout')
 
+<style>
+    .box{
+        background: transparent !important;
+        border: none !important;
+        box-shadow: none !important;
+    }
+</style>
+
 @section('header')
 	<section class="content-header">
 	  <h1>
@@ -24,10 +32,36 @@
       <div class="box-body" />
       <div class="row">
           @foreach ($properties as $property)
+          <div class="col-lg-3 col-sm-6 col-md-4">
+
+            <div class="card hovercard">
+                <div class="cardheader">
+                    <img src="https://media.timeout.com/images/103281049/630/472/image.jpg" height="100%" width="100%">
+                </div>
+                <div class="avatar">
+                    <img alt="" src="https://placehold.it/160x160/00a65a/ffffff/&text=1">
+                </div>
+                <div class="info">
+                    <div class="title">
+                        <a target="_blank" href="#">{{ $property->name }}</a>
+                    </div>
+                    <div class="desc">{{ $property->city }}</div>
+                </div>
+                <div class="bottom">
+                    <a class="btn btn-primary btn-sm" rel="publisher" href="#">
+                        Request
+                    </a>
+                </div>
+            </div>
+
+        </div>
+<!--
           <div class="col-lg-4 col-sm-6">
             {{ $property->name }}
+              <img src="">
             <a href="{{ url($crud->route) }}/watch/request/{{ $property->id }}"/>More Info</a>
           </div>
+-->
           @endforeach
       </div>
           <!-- this is the pagination -->
